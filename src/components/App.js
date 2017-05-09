4952400c396ef4ff656da9226373e6382747ef32
ali <ghanavatian.ali@gmail.com>
2017-05-09 18:40:57 +0430

    prepare codebase for ssr

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { Link } from 'react-router'
 
@@ -8,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="/logo.svg" className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <ul className="App-intro">
